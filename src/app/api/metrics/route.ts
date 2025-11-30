06ee51fad748e5a81a838b9c1297ba7b3dc6e815
BadiiMsalmi <msalmi.abdelbadii@tek-up.de>
2025-11-30 23:20:35 +0100

    adding metrics endpoint

---
+++ src/app/api/metrics/route.ts
@@ -0,0 +1,11 @@
+import client from 'prom-client';
+
+client.collectDefaultMetrics();
+
+export async function GET() {
+  const metrics = await client.register.metrics();
+  return new Response(metrics, {
+    status: 200,
+    headers: { 'Content-Type': client.register.contentType },
+  });
+}
